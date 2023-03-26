230aebd8b2824ad5cace193f144a54c64267454a
ccosner <cmcosner@gmail.com>
2023-03-26 09:14:32 -0700

    update

--- EERE/src/js/scripts.js
+++ EERE/src/js/scripts.js
@@ -1,41 +1,44 @@
-const wpArr = document.querySelectorAll('.wp');
-const bodyClass = document.body.className;
+window.addEventListener('load', () => {
+    const rellax = new Rellax('.rellax');
+    const wpArr = document.querySelectorAll('.wp');
 
-const setBodyClass = (bodyClass) => {
-    document.body.className = bodyClass;
-};
+    const animateCSS = (node, animation, prefix = 'animate__') =>
+        new Promise((resolve, reject) => {
+            const animationName = `${prefix}${animation}`;
 
-const animateCSS = (node, animation, prefix = 'animate__') =>
-    new Promise((resolve, reject) => {
-        const animationName = `${prefix}${animation}`;
+            node.classList.add(`${prefix}animated`, animationName);
 
-        node.classList.add(`${prefix}animated`, animationName);
-
-        function handleAnimationEnd(event) {
-            event.stopPropagation();
-            node.classList.remove(`${prefix}animated`, animationName);
-            resolve('Animation ended');
-        }
-
-        node.addEventListener('animationend', handleAnimationEnd, { once: true });
-    });
-
-wpArr.forEach((wp, i) => {
-    const wpId = 's_' + i;
-    const prevWpId = 's_' + (i - 1);
-    wp.id = wpId;
-
-    new Waypoint({
-        element: wp,
-        handler: function (direction) {
-            if (direction === 'down') {
-                setBodyClass(wpId);
-            } else {
-                setBodyClass(prevWpId);
+            function handleAnimationEnd(event) {
+                event.stopPropagation();
+                //node.classList.remove(`${prefix}animated`, animationName);
+                resolve('Animation ended');
             }
-        },
-        offset: '50%'
+
+            node.addEventListener('animationend', handleAnimationEnd, { once: true });
+        });
+
+    wpArr.forEach((wp, i) => {
+        wp.classList.add('hidden');
+        const wpId = 'wp_' + i;
+        wp.id = wpId;
+
+        new Waypoint({
+            element: wp,
+            handler: function (direction) {
+                if (direction === 'down') {
+                    wp.classList.remove('hidden');
+                    if (wp.classList.contains('box')) {
+                        animateCSS(wp, 'zoomIn');
+                    } else if (wp.classList.contains('bug-light')) {
+                        animateCSS(wp, 'flash');
+                    } else {
+                        wp.childNodes.forEach((el) => {
+                            animateCSS(el, 'fadeInUp');
+                        });
+                    }
+                }
+            },
+            offset: '80%'
+        });
     });
 });
-
-const rellax = new Rellax('.rellax');
